6d970238ae8680ebf91d1e1d6df9058f4f46aba9
HimanshuRanjan1 <himanshu02ranjan@gmail.com>
2023-02-16 06:20:57 +0530

    minor

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const socketIo = require('socket.io')
 const server = http.createServer(app)
 const io = socketIo(server,{ 
     cors: {
-      origin: 'http://localhost:3000'
+      origin: '*'
     }
 })
 
